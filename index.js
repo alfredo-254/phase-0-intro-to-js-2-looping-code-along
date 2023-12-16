313473f2844d40ec04fa723e5446ec460e1e97ae
alfred kivuva <kivuvakioko97@gmail.com>
2023-12-16 23:43:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names=(["Guadalupe", "Ollie", "Aki"], surprise)
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const thankYouMessage = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(thankYouMessage);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  
+  // Function to count down from a given number and log each number
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
